adfeb16af3c32415af8c41d2a5b259b471c7f111
Jake Green <jgreen47@byui.edu>
2019-11-18 14:15:03 -0600

    Following reading to connect/query database

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
+const connectionString = process.env.DATABASE_URL || "postgres://urumsnimhestzf:ed9d4f490a23a8e9d1474c84d366681357010b19e3ef307b7204bdb072289c0c@ec2-174-129-253-144.compute-1.amazonaws.com:5432/dfvbbt9ju2co2t?ssl=true";
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/dbConnect', function(req, res){
+    const pool = new Pool({connectionString: connectionString});
+    var sql = "Select * FROM test_table";
+    pool.query(sql, function(err, result) {
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+      }
+      console.log("Back from DB with result:");
+      console.log(result.rows);
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
