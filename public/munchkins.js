7106eddc9b1a5f905be9d1fa613043016c32170e
Jake Green <jgreen47@byui.edu>
2019-11-20 15:23:03 -0600

    Testing AJAX query to database.

--- public/munchkins.js
+++ public/munchkins.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#button").click(function(){
     $.getJSON("/dbConnect", function(result){
       $.each(result, function(i, field){
-        $("#response").append(field + " ");
+        $("#response").append(field.name + " ");
       });
     });
   });
