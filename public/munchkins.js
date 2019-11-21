c75376466a51c7b1a90ccdfc972fa1fca933e434
Jake Green <jgreen47@byui.edu>
2019-11-21 12:38:51 -0600

    adding buttons to the table.

--- public/munchkins.js
+++ public/munchkins.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
     $("#response").html("<table class='table table-bordered'>");
     $.getJSON("/dbConnect", function(result){
       $.each(result, function(i, field){
-        var resp = "<tr><td>Name: " + field.name;
-        resp = resp + '<button type="button" name="button" class="remove-player" id="' + field.id + '">Remove Player</button></td>';
+        var resp = "<tr><td>Name: " + field.name + "</td>";
         resp = resp + "<td>Gender:" + field.gender + "</td>";
         resp = resp + "<td>Level: " + field.level + "</td>" + "<td>Equipment:" + field.equipment + "</td>";
+        resp = resp + '<td><button type="button" name="button" class="remove-player" id="' + field.id + '">Remove Player</button></td>';
         resp = resp + "</tr>";
         $("#response").append(resp);
       });
