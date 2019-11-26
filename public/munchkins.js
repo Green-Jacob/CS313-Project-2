f71db423de087fda1f4cdbfbcf263398111d9d5c
Jake Green <jgreen47@byui.edu>
2019-11-25 19:01:22 -0600

    chasing bug of syntax

--- public/munchkins.js
+++ public/munchkins.js
@@ -6,17 +6,17 @@ $(document).ready(function(){
 //gets all rows from database and loads it into the table.
 function getPlayers(){
   $(document).ready(function(){
-  $("#response").html("<table class='table table-bordered'>");
-  $.getJSON("/dbConnect", function(result){
-    $.each(result, function(i, field){
-      var resp = "<tr><td>Name: " + field.name + "</td>";
-      resp = resp + "<td>Gender:" + field.gender + "</td>";
-      resp = resp + "<td>Level: " + field.level + "</td>" + "<td>Equipment:" + field.equipment + "</td>";
-      resp = resp + '<td><button type="button" name="button" class="remove-player" id="' + field.id + '">Remove Player</button></td>';
-      resp = resp + "</tr>";
-      $("#response").append(resp);
+    $("#response").html("<table class='table table-bordered'>");
+    $.getJSON("/dbConnect", function(result){
+      $.each(result, function(i, field){
+        var resp = "<tr><td>Name: " + field.name + "</td>";
+        resp = resp + "<td>Gender:" + field.gender + "</td>";
+        resp = resp + "<td>Level: " + field.level + "</td>" + "<td>Equipment:" + field.equipment + "</td>";
+        resp = resp + '<td><button type="button" name="button" class="remove-player" id="' + field.id + '">Remove Player</button></td>';
+        resp = resp + "</tr>";
+        $("#response").append(resp);
+      });
     });
-  });
-$("#response").append("</table>");
-});
+    $("#response").append("</table>");
+    });
 }
