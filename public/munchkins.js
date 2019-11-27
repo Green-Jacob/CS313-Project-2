ed8789ba25c82d1bac95cd44e20c5d4e0c33f6b0
Jake Green <jgreen47@byui.edu>
2019-11-26 20:45:11 -0600

    removing players

--- public/munchkins.js
+++ public/munchkins.js
@@ -23,7 +23,7 @@ $.getPlayers = function(){
       var resp = "<tr><td>Name: " + field.name + "</td>";
       resp = resp + "<td>Gender:" + field.gender + "</td>";
       resp = resp + "<td>Level: " + field.level + "</td>" + "<td>Equipment:" + field.equipment + "</td>";
-      resp = resp + '<td><button type="button" name="button" class="remove" value="' + field.id + '" onclick="remove()">Remove Player</button></td>';
+      resp = resp + '<td><button type="button" name="remove" value="' + field.id + '" onclick="remove()">Remove Player</button></td>';
       resp = resp + "</tr>";
       $("#response").append(resp);
     });
@@ -47,7 +47,7 @@ $.addPlayer = function(){
 };
 
 function remove() {
-  $(".remove").click(function(){
+  $("[name='remove']").click(function(){
     console.log(this.value);
   });
-}
+};
