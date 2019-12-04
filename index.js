da9fa5b4f9601e737a6f737c9b667a5186cbafe4
Jake Green <jgreen47@byui.edu>
2019-12-03 18:39:28 -0600

    Added more buttons with delegated event handlers.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || 'postgres://urumsnimhestzf:ed9d4f490a23a8e9d1474c84d366681357010b19e3ef307b7204bdb072289c0c@ec2-174-129-253-144.compute-1.amazonaws.com:5432/dfvbbt9ju2co2t?ssl=true';
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,7 +12,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/dbConnect', function(req, res){
     const pool = new Pool({connectionString: connectionString});
-    var sql = "Select * FROM players";
+    var sql = "Select * FROM players ORDER BY id";
     pool.query(sql, function(err, result) {
       if (err) {
         console.log("Error in query: ");
@@ -42,7 +42,7 @@ express()
   })
   .get('/removePlayer', function(req, res){
     var id = req.query.id;
-    console.log("ID: " + id);
+    console.log("Remove ID: " + id);
     const pool = new Pool({connectionString: connectionString});
     var sql = "DELETE FROM players WHERE id='" + id + "'";
     pool.query(sql, function(err, result) {
@@ -53,5 +53,37 @@ express()
       }
       res.send(true);
     });
+  })
+  .get('/updateLevel', function(req, res){
+    var id = req.query.id;
+    var newLevel = req.query.newLevel;
+    console.log("Update Level ID: " + id);
+    const pool = new Pool({connectionString: connectionString});
+    var sql = "UPDATE players SET level = " + newLevel + " WHERE id = " + id;
+    console.log("Update Level SQL: " + sql);
+    pool.query(sql, function(err, result){
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+        res.send(false);
+      }
+      res.send(true);
+    })
+  })
+  .get('/updateEquipment', function(req, res){
+    var id = req.query.id;
+    var newEquipment = req.query.newEquipment;
+    console.log("Update Level ID: " + id);
+    const pool = new Pool({connectionString: connectionString});
+    var sql = "UPDATE players SET equipment = " + newEquipment + " WHERE id = " + id;
+    console.log("Update Equipment SQL: " + sql);
+    pool.query(sql, function(err, result){
+      if (err) {
+        console.log("Error in query: ");
+        console.log(err);
+        res.send(false);
+      }
+      res.send(true);
+    })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/munchkins.js
+++ public/munchkins.js
@@ -1,29 +1,77 @@
+/**
+Binds event handlers to the Buttons
+**/
 $(document).ready(function(){
+  $.getPlayers();
   $("#get-players").click(function(){
     $.getPlayers();
   });
   $("#add-player").click(function(){
     $.addPlayer();
   });
+  $("table").on("dblclick", "[name='remove']",function(event){
+    let id = $(this).attr('id');
+    remove(id);
+  });
+  $("table").on("click", "[name='level-up']",function(event){
+    let action = $(this).attr('name');
+    let id = $(this).attr('id');
+    let level = $(this).val();
+    updateLevel(action, id, level);
+  });
+  $("table").on("click", "[name='level-down']",function(event){
+    let action = $(this).attr('name');
+    let id = $(this).attr('id');
+    let level = $(this).val();
+    updateLevel(action, id, level);
+  });
+  $("table").on("click", "[name='equip-up']",function(event){
+    let action = $(this).attr('name');
+    let id = $(this).attr('id');
+    let equip = $(this).val();
+    console.log("Equip-up " + action + " " + id + " " + equip);
+    updateEquipment(action, id, equip);
+  });
+  $("table").on("click", "[name='equip-down']",function(event){
+    let action = $(this).attr('name');
+    let id = $(this).attr('id');
+    let equip = $(this).val();
+    console.log("Equip-down " + action + " " + id + " " + equip);
+    updateEquipment(action, id, equip);
+  });
 });
-//retrieves rows from the database and displays it in a table
+/**
+retrieves rows from the database and displays it in a table
+**/
 $.getPlayers = function(){
-  $("#response").empty();
+  //$("#response").empty();
   $("#response").html("<table class='table table-bordered'>");
   $.getJSON("/dbConnect", function(result){
     $.each(result, function(i, field){
+      let lvlU = levelUpButton(field.level, field.id);
+      let lvlD = levelDownButton(field.level, field.id);
+      let equipU = equipUpButton(field.equipment, field.id);
+      let equipD = equipDownButton(field.equipment, field.id);
+      let genS = genderButton(field.gender, field.id);
+      let removeP = removePlayerButton(field.id);
+      let total = (Number(field.level) + Number(field.equipment));
+      //let str = lvlU + "/n" + lvlD + "/n" + equipU + "/n" + equipD + "/n" + genS;
+      //console.log("Generated Buttons: " + str);
       var resp = "<tr><td>Name: " + field.name + "</td>";
-      resp = resp + "<td>Gender:" + field.gender + "</td>";
-      resp = resp + "<td>Level: " + field.level + "</td>" + "<td>Equipment:" + field.equipment + "</td>";
-      resp = resp + '<td><button type="button" name="remove" value="' + field.id;
-      resp = resp + '" onclick="remove()">Remove Player (double click)</button></td>';
+      resp = resp + "<td>Combat Level: " + total + "</td>";
+      resp = resp + "<td>Level: " + field.level + lvlU + " " + lvlD +"</td>";
+      resp = resp + "<td>Equipment: " + field.equipment + " " + equipU + " " + equipD + "</td>";
+      resp = resp + "<td>Gender: " + field.gender + " " + genS + "</td>";
+      resp = resp + "<td>" + removeP + "</td>";
       resp = resp + "</tr>";
       $("#response").append(resp);
     });
   });
   $("#response").append("</table>");
 };
-//adds player to the database
+/**
+adds player to the database
+**/
 $.addPlayer = function(){
   var i = false;
   var name = $("#name").val();
@@ -39,18 +87,106 @@ $.addPlayer = function(){
   });
   //alert("Player " + name + "added. Please load players.");
 };
+/**
+Removes the player from the database using the players id
+**/
+function remove(id) {
+  if (id == undefined) {
+    console.log("Undefined ID sent to remove()");
+    return;
+  }
+  console.log("Remove player function ID: " + id);
+  var url = "/removePlayer?id=" + id;
+  console.log(url);
+  $.get(url, function(result){
+    console.log(result);
+    $.getPlayers();
+  })
+};
+/**
+Updates the players level based on which button is pressed
+**/
+function updateLevel(action, id, level){
+  if (action == undefined || id == undefined || level == undefined) {
+    console.log("Update Level attempted with undefined value");
+    return;
+  }
+  switch (action) {
+    case 'level-up':
+      level++;
+      break;
+    case 'level-down':
+      if (level <= 1) {
+        level = 1;//game rules say players level can't go lower than 1
+      }
+      else {
+        level--
+      }
+      break;
+    default:
 
-function remove() {
-  $("document").ready(function(){
-    $("[name='remove']").click(function(){
-      var id = this.value;
-      console.log(this.value);
-      url = "/removePlayer?id=" + id;
-      console.log(url);
-      $.get(url, function(result){
-        console.log(result);
-        $.getPlayers();
-      })
-    });
-  });
+  }
+  console.log("New Level: " + level);
+  var url = "/updateLevel?id=" + id + "&newLevel=" + level;
+  $.get(url, function(result){
+    console.log(result);
+    $.getPlayers();
+  })
+};
+/**
+Updates the players equipment based on which button is pressed
+**/
+function updateEquipment(action, id, equip){
+  if (action == undefined || id == undefined || equip == undefined) {
+    console.log("Update Equipment attempted with undefined value");
+    return;
+  }
+  switch (action) {
+    case 'equip-up':
+      equip++;
+      break;
+    case 'equip-down':
+      if (equip <= 0) {
+        equip = 0;//game rules say players equipment can't go lower than 0
+      }
+      else {
+        equip--
+      }
+      break;
+    default:
+
+  }
+  console.log("New Equipment: " + equip);
+  var url = "/updateEquipment?id=" + id + "&newEquipment=" + equip;
+  $.get(url, function(result){
+    console.log(result);
+    $.getPlayers();
+  })
+};
+/**
+These functions generate strings with the HTML for the various buttons
+**/
+function levelUpButton(level, id){
+  var button = "<button type='button' name='level-up' id='" + id + "' value='" + level + "'>Up</button>";
+  return button;
+};
+function levelDownButton(level, id) {
+  var button = "<button type='button' name='level-down' id='" + id + "' value='" + level + "'>Down</button>";
+  return button;
+};
+function equipUpButton(equipment, id){
+  var button = "<button type='button' name='equip-up' id='" + id + "' value='" + equipment + "'>Up</button>";
+  return button;
+};
+function equipDownButton(equipment, id){
+  var button = "<button type='button' name='equip-down' id='" + id + "' value='" + equipment + "'>Down</button>";
+  return button;
+};
+function genderButton(gender, id){
+  var button = "<button type='button' name='gender-swap' id='" + id + "' value='" + gender + "'>Swap</button>";
+  return button;
+};
+function removePlayerButton(id){
+  var button = "<button type='button' name='remove' id='" + id + "'>Remove Player (double click)</button>";
+  return button;
 };
